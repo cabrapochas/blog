fcef536ffccdce651ccf7b5a1ee681fbb9b14770
cabrapochas <cursomoocprueba@gmail.com>
2015-10-02 06:51:40 -0700

    Update models.py

--- blog/migrations/models.py
+++ blog/migrations/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 class Post(models.Model):
-author = models.ForeignKey(User)
-title = models.CharField(max_length=200)
-text = models.TextField()
-created_date = models.DateTimeField(default=timezone.now)
-published_date = models.DateTimeField(blank=True, null=True)
+ author = models.ForeignKey(User)
+ title = models.CharField(max_length=200)
+ text = models.TextField()
+ created_date = models.DateTimeField(default=timezone.now)
+ published_date = models.DateTimeField(blank=True, null=True)
 
 def publish(self):
     self.published_date = timezone.now()
